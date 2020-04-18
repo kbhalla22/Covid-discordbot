0111c01f42c6b60271b4ed03d973e89156b8a99b
karanbhalla <kbhalla22@gmail.com>
2020-04-18 19:09:03 -0400

    Initial commit

---
+++ src/main/java/com/karry/discordbot/covid/CovidBotApplication.java
@@ -0,0 +1,13 @@
+package com.karry.discordbot.covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karry/discordbot/covid/CovidBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karry.discordbot.covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
