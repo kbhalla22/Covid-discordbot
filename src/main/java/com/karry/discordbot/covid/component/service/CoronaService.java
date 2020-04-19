25e0a6295a072ee6f18f85231a3bc23745482ce1
karanbhalla <kbhalla22@gmail.com>
2020-04-18 21:17:01 -0400

    Adding service class

---
+++ src/main/java/com/karry/discordbot/covid/component/service/CoronaService.java
@@ -0,0 +1,48 @@
+package com.karry.discordbot.covid.component.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+
+
+@Service
+@Component
+@PropertySource("classpath:application.properties")
+public class CoronaService {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${karankey}")
+    public String APIKey;
+
+    @PostConstruct
+    public void getAllCountries(){
+
+        RestTemplate restTemplate=new RestTemplate();
+
+        String url="https://covid-193.p.rapidapi.com/countries";
+
+        HttpEntity<String>entity=new HttpEntity<String>("parameters",getHeaders());
+        ResponseEntity<String>response=restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
+System.out.println(response.getBody());
+        // https://covid-193.p.rapidapi.com/countries
+    }
+
+    private HttpHeaders getHeaders(){
+
+       HttpHeaders headers=new HttpHeaders();
+       headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("x-rapidapi-key",APIKey);
+
+        return headers;
+
+    }
+}
